6f680ce22d159a0b6c894be367c4af98c1c0e868
unknown <ka_soualehmohamed@esi.dz>
2023-03-16 14:58:29 +0100

    soa

--- examples/run/cuda/seq_example_cuda.cpp
+++ examples/run/cuda/seq_example_cuda.cpp
@@ -41,8 +41,8 @@
 
 
 using scalar = TRACCC_CUSTOM_SCALARTYPE;
-using uint_collection_types = collection_types<unsigned int>;
-using scalar_collection_types = collection_types<scalar>;
+using uint_collection_types = traccc::collection_types<unsigned int>;
+using scalar_collection_types = traccc::collection_types<scalar>;
 
 struct CellsBuffer {
     uint_collection_types::buffer   channel0;
