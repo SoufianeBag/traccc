08a8e256c79dd08bdbc58caf8aac46fa7e1d82d7
maissa <im_boukaf@esi.dz>
2023-03-20 22:44:35 -1100

    merge two kernels

--- device/common/include/traccc/clusterization/device/aggregate_cluster.hpp
+++ device/common/include/traccc/clusterization/device/aggregate_cluster.hpp
@@ -45,7 +45,7 @@ inline void aggregate_cluster2(
     const cell_module_collection_types::const_device& modules,
     const vecmem::data::vector_view<cluster> f_view,
     const unsigned int start, const unsigned int end, const unsigned short cid,
-    spacepoint& out, vecmem::data::vector_view<unsigned int> cell_links,
+    spacepoint_collection_types::device& spacepoints, vecmem::data::vector_view<unsigned int> cell_links,
     const unsigned int link);
 
 
