99d8b59a8b5135d97609f44fa989743609c5395f
maissa <im_boukaf@esi.dz>
2023-03-21 23:33:14 -1100

    version 1 + merge kernels

--- examples/run/cuda/seq_example_cuda.cpp
+++ examples/run/cuda/seq_example_cuda.cpp
@@ -166,7 +166,7 @@ int seq_run(const traccc::full_tracking_input_config& i_cfg,
                 spacepoints_cuda_buffer =
                     ca_cuda(cells_buffer, modules_buffer , cellsSoA).first;
                 stream.synchronize();
-                printf("num measur : %u \n",ca_cuda(cells_buffer, modules_buffer , cellsSoA).second);
+                printf("num measur : %llu \n",ca_cuda(cells_buffer, modules_buffer , cellsSoA).second);
             }  // stop measuring clusterization cuda timer
 
             if (run_cpu) {
